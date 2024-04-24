4fec432670a3e3c2a0057c0f4b8cdadb01760b3b
Rafał Klusek <klusek.raf@gmail.com>
2024-04-25 01:31:58 +0200

    tidy

--- server/src/services/auth.rs
+++ server/src/services/auth.rs
@@ -2,9 +2,7 @@ pub mod grpc {
     tonic::include_proto!("auth");
 }
 
-pub struct Service {
-
-}
+pub struct Service {}
 
 impl Service {
     pub fn new() -> Self {
@@ -18,7 +16,10 @@ impl Service {
 
 #[tonic::async_trait]
 impl grpc::auth_server::Auth for Service {
-    async fn login(&self, request: tonic::Request<grpc::LoginRequest>) -> Result<tonic::Response<grpc::LoginResponse>, tonic::Status> {
+    async fn login(
+        &self,
+        request: tonic::Request<grpc::LoginRequest>,
+    ) -> Result<tonic::Response<grpc::LoginResponse>, tonic::Status> {
         let response = grpc::LoginResponse {
             token: request.into_inner().username,
         };
