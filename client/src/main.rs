1c49d2edfeb5a358e2aa3316e315e89530f65c8e
Rafał Klusek <klusek.raf@gmail.com>
2024-06-03 00:10:28 +0200

    macroquad ui

---
+++ client/src/main.rs
@@ -0,0 +1,40 @@
+use macroquad::prelude::*;
+use macroquad::ui::{
+    Drag, hash,
+    root_ui,
+    Ui, widgets::{self, Group},
+};
+use macroquad::ui::widgets::{Button, InputText};
+use macroquad::window::Conf;
+
+fn window_conf() -> Conf {
+    Conf {
+        window_title: "Playmat".to_owned(),
+        window_width: 1920,
+        window_height: 1080,
+        fullscreen: false,
+        ..Default::default()
+    }
+}
+
+#[macroquad::main(window_conf)]
+async fn main() {
+    let mut username = String::new();
+    let mut password = String::new();
+    loop {
+        macroquad::window::clear_background(macroquad::color::BLACK);
+        widgets::Window::new(hash!(), vec2(400., 200.), vec2(320., 400.))
+            .label("Register")
+            .titlebar(false)
+            .movable(false)
+            .ui(&mut *root_ui(), |ui| {
+                InputText::new(hash!()).label("Username").ui(ui, &mut username);
+                InputText::new(hash!()).label("Password").password(true).ui(ui, &mut password);
+                if Button::new("Register").ui(ui) {
+                    println!("Registering...");
+                }
+            });
+        macroquad::text::draw_text("Hello, world!", 20.0, 20.0, 30.0, macroquad::color::BLACK);
+        macroquad::window::next_frame().await
+    }
+}
