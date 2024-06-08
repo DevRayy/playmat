8c1d6e94b0117526c4760462292877d0bceb5784
Rafał Klusek <klusek.raf@gmail.com>
2024-06-08 02:13:13 +0200

    register through client

---
+++ client/src/client.rs
@@ -0,0 +1,24 @@
+pub mod grpc {
+    tonic::include_proto!("auth");
+}
+
+pub struct AuthService {
+    client: grpc::auth_client::AuthClient<tonic::transport::Channel>,
+}
+
+impl AuthService {
+    pub async fn new() -> Self {
+        let client = grpc::auth_client::AuthClient::connect("http://127.0.0.1:9090").await.unwrap();
+        Self { client }
+    }
+
+    pub async fn register(&mut self, email: String, password: String) -> Result<(), tonic::Status> {
+        let request = tonic::Request::new(grpc::RegisterRequest {
+            email,
+            password,
+        });
+
+        self.client.register(request).await?;
+        Ok(())
+    }
+}
--- client/src/main.rs
+++ client/src/main.rs
@@ -1,3 +1,5 @@
+use std::sync::Arc;
+
 use macroquad::prelude::*;
 use macroquad::ui::widgets::{Button, InputText};
 use macroquad::ui::{
@@ -5,6 +7,10 @@ use macroquad::ui::{
     widgets::{self},
 };
 use macroquad::window::Conf;
+use tokio::sync::Mutex;
+// use tokio::task::JoinHandle;
+
+mod client;
 
 fn window_conf() -> Conf {
     Conf {
@@ -18,8 +24,19 @@ fn window_conf() -> Conf {
 
 #[macroquad::main(window_conf)]
 async fn main() {
+    let rt = tokio::runtime::Builder::new_multi_thread()
+        .worker_threads(4)
+        .enable_all()
+        .build()
+        .unwrap();
+
+    let auth = rt.block_on(async {
+        Arc::new(Mutex::new(client::AuthService::new().await))
+    });
     let mut username = String::new();
     let mut password = String::new();
+    let mut clicked = false;
+
     loop {
         macroquad::window::clear_background(macroquad::color::BLACK);
         widgets::Window::new(hash!(), vec2(400., 200.), vec2(320., 400.))
@@ -34,10 +51,17 @@ async fn main() {
                     .label("Password")
                     .password(true)
                     .ui(ui, &mut password);
-                if Button::new("Register").ui(ui) {
-                    println!("Registering...");
+                if !clicked && Button::new("Register").ui(ui) {
+                    clicked = true;
                 }
             });
+            if clicked == true {
+                rt.block_on(async{
+                    let mut auth = auth.lock().await;
+                    auth.register(username.clone(), password.clone()).await.unwrap();
+                    clicked = false;
+                });
+            }
         macroquad::text::draw_text("Hello, world!", 20.0, 20.0, 30.0, macroquad::color::BLACK);
         macroquad::window::next_frame().await
     }
